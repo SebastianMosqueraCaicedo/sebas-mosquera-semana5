58eed45e9bdc805def21cbdd567abff2b2f5d660
tabo <1005893565@u.icesi.edu.com>
2022-09-08 14:30:01 -0500

    add finishing touches

--- main.js
+++ main.js
@@ -12,12 +12,15 @@ const days_element = document.querySelector('.date-picker .dates .days');
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+// varables using the Date() format
 let date = new Date();
 let day = date.getDate();
 let month = date.getMonth();
 let year = date.getFullYear();
 
 let selectedDate = date;
+
+// array containing first and last day selected
 let selectedDay = [
 	{day: day, month: month, year: year},
 	{day: 1, month: month, year: year}
@@ -26,6 +29,7 @@ let selectedDay = [
 let selectedMonth = month;
 let selectedYear = year;
 
+// array containing the items in between the day selection
 let insideArray = [0];
 
 mth_element.textContent = months[month] + ' ' + year;
@@ -41,12 +45,14 @@ next_mth_element.addEventListener('click', goToNextMonth);
 prev_mth_element.addEventListener('click', goToPrevMonth);
 
 // FUNCTIONS
+// extends the "menu"
 function toggleDatePicker (e) {
 	if (!checkEventPathForClass(e.path, 'dates')) {
 		dates_element.classList.toggle('active');
 	}
 }
 
+// increases the month and redraws the dates within
 function goToNextMonth (e) {
 	month++;
 	if (month > 11) {
@@ -67,6 +73,7 @@ function goToPrevMonth (e) {
 	populateDates();
 }
 
+// draws the dates and assigns their css state
 function populateDates(e) {
 	days_element.innerHTML = '';
 	let amount_days = 31;
@@ -96,7 +103,7 @@ function populateDates(e) {
 		day_element.classList.add('day');
 		day_element.textContent = i + 1;
 
-		// selects the outher ranges of the interval
+		// selects the outer ranges of the interval
 		if (selectedDay[1].day === (i + 1) && selectedDay[1].year === year && selectedDay[1].month === month) {
 			day_element.classList.add('selected-first');
 		}
@@ -104,7 +111,6 @@ function populateDates(e) {
 		if (selectedDay[0].day === (i + 1) && selectedDay[0].year === year && selectedDay[0].month === month) {
 			day_element.classList.add('selected-last');
 		}
-
 
 		day_element.addEventListener('click', function () {
 			selectedDate = new Date(year + '-' + (month + 1) + '-' + (i + 1));
@@ -119,7 +125,8 @@ function populateDates(e) {
 			selectedMonth = month;
 			selectedYear = year;
 
-			selected_date_element.textContent = formatDate(selectedDate);
+			// shows the selected dates as text
+			selected_date_element.textContent = selectedDay[1].day + ' / ' + selectedDay[1].month + ' / ' + selectedDay[1].year + ' - ' + selectedDay[0].day + ' / ' + selectedDay[0].month + ' / ' + selectedDay[0].year ;
 			selected_date_element.dataset.value = selectedDate;
 
 			populateDates();
@@ -134,12 +141,16 @@ function populateDates(e) {
 			}
 		}
 
-			if (selectedDay[1].month < selectedMonth && selectedDay[1].day < i) {
-					day_element.classList.add('selected-inside');
-				console.log(i);
-				console.log('month ',selectedDay[1].month);
-				console.log('day ',selectedDay[1].day);
-			}
+		/* if the final date is in a different month,
+		 * highlight all days up to that date
+		 *
+		if (selectedDay[1].month < selectedMonth && selectedDay[1].day < i) {
+				day_element.classList.add('selected-inside');
+			console.log(i);
+			console.log('month ',selectedDay[1].month);
+			console.log('day ',selectedDay[1].day);
+		}
+		*/
 
 		days_element.appendChild(day_element);
 //		console.log(insideArray);
@@ -158,6 +169,7 @@ function checkEventPathForClass (path, selector) {
 	return false;
 }	
 
+// visibility for the days
 function formatDate (d) {
 	let day = d.getDate();
 	if (day < 10) {
